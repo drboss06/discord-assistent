6177a1bc296cd640a0d37e8c2a950bc66c19862f
Zeldini <s4sha228@yandex.ru>
2024-05-23 10:44:41 +0000

    recognition stt

--- test_bot/extend/speach.py
+++ test_bot/extend/speach.py
@@ -1,50 +1,20 @@
-import speech_recognition as sr
 from subprocess import Popen
-import vosk
-import wave
-import json
+import speech_recognition as sr
 
-file_path = "test_bot/recordings/331453981782048768-1716297355316.ogg"
-file_path2 = "test_bot/recordings/331453981782048768-1716297355316.wav"
+
+file_path = "/workspaces/assist/test_bot/recordings/331453981782048768-1716313968713.ogg"
+file_path2 = "/workspaces/assist/test_bot/recordings/331453981782048768-1716313968713.wav"
+file_path3 = "/workspaces/assist/test_bot/recordings/331453981782048768-1716313968713.mp3"
 
 def ogg_to_wav(file):
-        args = ['ffmpeg','-i', file, file_path2]
-        process = Popen(args)
-        process.wait()
+    args = ['ffmpeg','-i', file, file_path3]
+    process = Popen(args)
+    process.wait()
 
-# r = sr.Recognizer()
-# #ogg_to_wav(file_path)
+r = sr.Recognizer()
 
-# with sr.AudioFile(file_path2) as source:
-#     audio = r.record(source)  # read the entire audio file
-#     text  = r.recognize_google(audio)
-#     print(text)
 
-model = vosk.Model("test_bot/extend/vosk-model-small-ru-0.22")
-
-wf = wave.open(file_path2, "rb")
-rec = vosk.KaldiRecognizer(model, 8000)
-
-result = ''
-last_n = False
-
-while True:
-        data = wf.readframes(8000)
-        if len(data) == 0:
-                break
-
-        if rec.AcceptWaveform(data):
-                res = json.loads(rec.Result())
-
-        if res['text'] != '':
-                result += f" {res['text']}"
-                last_n = False
-        elif not last_n:
-                result += '\n'
-                last_n = True
-
-res = json.loads(rec.FinalResult())
-result += f" {res['text']}"
-
-print(result)
-
+with sr.AudioFile(file_path2) as source:
+    audio_data = r.record(source)
+    text = r.recognize_google(audio_data, language="ru-RU")
+    print(text)
